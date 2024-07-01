39e35c5513c6923aa028d8d63164b23d873a1a6a
ANILKUMARARI <anilkumarari123.com>
2024-07-01 12:15:49 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        working with primitives
+         */
+        byte age;
+        age = 35;
+        System.out.println( "Age : " + age);
+
+        short orderValue ;
+        orderValue = 5433;
+        System.out.println ("Order value : " +orderValue);
+    }
+}
